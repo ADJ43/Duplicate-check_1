780dbaaf0af91a6c0af77dedcd3086c0fb4c845d
ADJ43 <adjse43@gmail.com>
2023-12-17 20:59:49 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duplicate_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Write a program and ask the user to enter a few numbers separated by a hyphen.
+            //If the user simply presses Enter, without supplying an input, exit immediately;
+            //otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.
+
+            List<int> list = new List<int>();
+            Console.WriteLine("Please enter numbers separated by '-'");
+            string input = Console.ReadLine();
+            if (input == null)
+            {
+                Environment.Exit(0);
+            }
+            var splitInput = input.Split('-');
+
+
+            for (int i = 0; i < splitInput.Length; i++)
+            {
+                list.Add(int.Parse(splitInput[i]));
+
+            }
+            var index = 0;
+            foreach (var number in list)
+            {
+                if (list.IndexOf(number) != index || list.LastIndexOf(number) != index)
+                {
+                    Console.WriteLine("duplicate");
+                    break;
+                }
+
+                index = index + 1;
+            }
+
+
+        }
+    }
+}
